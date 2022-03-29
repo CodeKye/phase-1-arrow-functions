b6f663899078f6a81ea5e9b98311726d04a09bd2
David Kye <davidkye2.0@gmail.com>
2022-03-29 17:31:10 -0400

    commit tree 9fd17a63b786c2eb1ecb0583dae324c96f2c0be2

---
+++ index.js
@@ -0,0 +1,15 @@
+//function divide (num1, num2){
+//    console.log (num1 / num2)
+//}
+
+const divide = () => {
+    const num1 = 2000
+    const num2 = 100
+    return num1 / num2
+}
+
+const square = (num) => num * num
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
